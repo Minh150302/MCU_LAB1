5a593f33e2bd958e7238be9b6484d90b97f7718c
LAPTOP-MINH\Huy Minh <hoanghuyminh1503@gmail.com>
2023-09-25 09:52:28 +0700

    COMMIT EX3

--- LAB1/Core/Inc/main.h
+++ LAB1/Core/Inc/main.h
@@ -58,10 +58,18 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED_RED_Pin GPIO_PIN_5
-#define LED_RED_GPIO_Port GPIOA
-#define LED_YELLOW_Pin GPIO_PIN_6
-#define LED_YELLOW_GPIO_Port GPIOA
+#define LED_RED_V_Pin GPIO_PIN_2
+#define LED_RED_V_GPIO_Port GPIOA
+#define LED_YELLOW_V_Pin GPIO_PIN_3
+#define LED_YELLOW_V_GPIO_Port GPIOA
+#define LED_GREEN_V_Pin GPIO_PIN_4
+#define LED_GREEN_V_GPIO_Port GPIOA
+#define LED_RED_H_Pin GPIO_PIN_5
+#define LED_RED_H_GPIO_Port GPIOA
+#define LED_YELLOW_H_Pin GPIO_PIN_6
+#define LED_YELLOW_H_GPIO_Port GPIOA
+#define LED_GREEN_H_Pin GPIO_PIN_7
+#define LED_GREEN_H_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
